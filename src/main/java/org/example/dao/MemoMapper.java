b4c2ffca03db8e531031ad97f570f1a9fe0fc2ed
kaeback <kaeback@gmail.com>
2022-12-19 20:51:23 +0900

    12/19 코드 추가

--- src/main/java/org/example/dao/MemoMapper.java
+++ src/main/java/org/example/dao/MemoMapper.java
@@ -1,9 +1,11 @@
 package org.example.dao;
 
 import org.example.model.Member;
+import org.example.model.Memo;
 import org.example.model.ReceiveMemoRecord;
 import org.example.model.SendMemoRecord;
 
+import java.util.List;
 import java.util.Map;
 
 public interface MemoMapper {
@@ -13,6 +15,9 @@ public interface MemoMapper {
     // 로그인
     Member findMember(Member member);
 
+    // 메모 저장
+    int saveMemo(Memo memo);
+
     // 보낸 메모 저장
     int saveSendMemoRecord(SendMemoRecord sendMemoRecord);
 
@@ -20,16 +25,16 @@ public interface MemoMapper {
     int saveReceiveMemoRecord(ReceiveMemoRecord receiveMemoRecord);
 
     // 보낸 메모 읽기
-    SendMemoRecord findSendMemoRecordById(long send_id);
+    Map<String, Object> findSendMemoRecordById(long send_id);
 
     // 받은 메모 읽기
-    ReceiveMemoRecord findReceiveMemoRecordById(long receive_id);
+    Map<String, Object> findReceiveMemoRecordById(long receive_id);
 
     // 보낸 메모 전체목록(검색)
-    Map<String, Object> findSendMemoRecords(String title);
+    List<Map<String, Object>> findSendMemoRecords(String title);
 
     // 받은 메모 전체목록(검색)
-    Map<String, Object> findReceiveMemoRecords(String title);
+    List<Map<String, Object>> findReceiveMemoRecords(String title);
 
     // 받은 메모 삭제
     int removeReceiveMemoRecordById(long receive_id);
